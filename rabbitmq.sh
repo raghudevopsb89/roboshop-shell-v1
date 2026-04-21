e0796da2906564e662fbbe59ae96f3a8cdd38c69
Raghu K <raghuk.vit@gmail.com>
2026-04-21 05:57:54 +0530

    RoboShop Code

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -1,22 +1,6 @@
-cat > /etc/yum.repos.d/rabbitmq_erlang.repo << 'EOF'
-[rabbitmq_erlang]
-name=rabbitmq_erlang
-baseurl=https://packagecloud.io/rabbitmq/erlang/el/9/$basearch
-gpgcheck=0
-enabled=1
-EOF
+cp rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo
 
-dnf install -y erlang
-
-cat > /etc/yum.repos.d/rabbitmq_rabbitmq-server.repo << 'EOF'
-[rabbitmq_rabbitmq-server]
-name=rabbitmq_rabbitmq-server
-baseurl=https://packagecloud.io/rabbitmq/rabbitmq-server/el/9/$basearch
-gpgcheck=0
-enabled=1
-EOF
-
-dnf install -y rabbitmq-server
+dnf install -y rabbitmq-server erlang
 systemctl enable rabbitmq-server
 systemctl start rabbitmq-server
 
