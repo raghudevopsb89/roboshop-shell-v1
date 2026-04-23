d1dcfb0466d435424dc88f9f1802bf4645b8a54f
Raghu K <raghuk.vit@gmail.com>
2026-04-23 22:11:07 +0530

    RoboShop Code

--- mongodb.sh
+++ mongodb.sh
@@ -1,10 +1,4 @@
-cat > /etc/yum.repos.d/mongodb-org-7.0.repo << 'EOF'
-[mongodb-org-7.0]
-name=MongoDB Repository
-baseurl=https://repo.mongodb.org/yum/redhat/9/mongodb-org/7.0/x86_64/
-gpgcheck=0
-enabled=1
-EOF
+cp mongo.repo /etc/yum.repos.d/mongo.repo
 
 dnf install -y mongodb-org
 systemctl enable mongod
